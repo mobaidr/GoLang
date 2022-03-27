d1437d62866a001d831a077e8684e2748838b522
Mohammed Rahman <mohammed_rahman@symbotic.com>
2022-03-27 16:54:45 +0530

    Added file test...

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -20,4 +23,34 @@ func TestNewDeck(t *testing.T) {
 	if actualStr != expectedStr {
 		t.Errorf("Expected %s, but got %s", expectedStr, actualStr)
 	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	filename := "_ desktesting"
+
+	os.Remove(filename)
+
+	deck := newDeck()
+
+	deck.saveToFile(filename)
+
+	loadedDeck := newDeckFrom(filename)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 20, but got %d", len(loadedDeck))
+	}
+
+	expectedStr := deck[0]
+	actualStr := loadedDeck[0]
+	if loadedDeck[0] != expectedStr {
+		t.Errorf("Expected %s, but got %s", expectedStr, actualStr)
+	}
+
+	expectedStr = deck[len(deck)-1]
+	actualStr = loadedDeck[len(loadedDeck)-1]
+	if actualStr != expectedStr {
+		t.Errorf("Expected %s, but got %s", expectedStr, actualStr)
+	}
+
+	os.Remove(filename)
 }
