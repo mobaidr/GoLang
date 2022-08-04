06a0aae9740b70782b96a2837fef3e2ecc84e524
obaid-eltropy <107457068+obaid-eltropy@users.noreply.github.com>
2022-08-04 16:43:10 +0530

    Initial structure of Udemy FinalProject

---
+++ UdemyFinalProject/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+const webPort = "80"
+func main() {
+	// Connect to a database
+
+	// Create sessions
+
+	// Create some channels
+
+	// create Wait group
+
+	// set up application config
+
+	// setup mail
+
+	// listen for web connections
+}
