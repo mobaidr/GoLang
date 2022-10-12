a6421f296987d1a6119d4a2590c58a430ea7cd86
obaid-eltropy <107457068+obaid-eltropy@users.noreply.github.com>
2022-10-12 19:25:35 +0530

    Setting up stub Mail microservice.

---
+++ microservices/mail-service/cmd/api/routes.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+package main
+
+import (
+"net/http"
+
+"github.com/go-chi/chi/v5"
+"github.com/go-chi/chi/v5/middleware"
+"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
