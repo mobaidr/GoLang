08fc0df3afefbefac1c312d366e53290108cbc88
obaid-eltropy <107457068+obaid-eltropy@users.noreply.github.com>
2023-01-02 16:35:58 +0530

    Listener stub added

--- microservices/listener-service/main.go
+++ microservices/listener-service/main.go
@@ -1,5 +1,8 @@
 package main
 
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 func main() {
 	// try to connect RabbitMQ
 
@@ -8,4 +11,11 @@ func main() {
 	// Create a consumer
 
 	// watch the queue  and consume events
+}
+
+func connect() (
+	*amqp.Connection,
+	error,
+) {
+	return nil, nil
 }
