42454cf9ba2b0d2a98f05e69012b903ad436aafd
obaid-eltropy <107457068+obaid-eltropy@users.noreply.github.com>
2023-03-10 16:43:22 +0530

    Testing Handlers: the Home handler

--- testing-course/webapp/cmd/web/handlers.go
+++ testing-course/webapp/cmd/web/handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"path"
 )
+
+var pathToTemplates = "./templates/"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
@@ -16,7 +19,7 @@ type TemplateData struct {
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
 	//Parse the template from the disk
-	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
 
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
---
+++ testing-course/webapp/cmd/web/handlers_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_applications_handlers(t *testing.T) {
+	var theTests = []struct {
+		name               string
+		url                string
+		expectedStatusCode int
+	}{
+		{"home", "/", http.StatusOK},
+		{"404", "/fish", http.StatusNotFound},
+	}
+
+	var app application
+	routes := app.routes()
+
+	pathToTemplates = "./../../templates/"
+
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTests {
+		resp, err := ts.Client().Get(ts.URL + e.url)
+
+		if err != nil {
+			t.Log(err)
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+		}
+	}
+}
